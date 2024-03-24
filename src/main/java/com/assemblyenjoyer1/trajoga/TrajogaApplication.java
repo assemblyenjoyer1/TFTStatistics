9f8153f21f035d4f44d03ebc3b33832c4ce758e9
assemblyenjoyer <T.Reiss@eos-ts.com>
2024-03-24 17:56:09 +0100

    first commit

---
+++ src/main/java/com/assemblyenjoyer1/trajoga/TrajogaApplication.java
@@ -0,0 +1,13 @@
+package com.assemblyenjoyer1.trajoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrajogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrajogaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assemblyenjoyer1/trajoga/TrajogaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assemblyenjoyer1.trajoga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrajogaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
